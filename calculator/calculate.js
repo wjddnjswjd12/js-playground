178c914272891ca06d9987657dc00ec0c61b3ae9
Jungwon Lee <leejungwon96@gmail.com>
2020-12-11 10:35:59 +0900

    wtever

--- calculator/calculate.js
+++ calculator/calculate.js
@@ -1,17 +1,8 @@
 function calculate() {
-  var nums = document.getElementById("input1");
-  var n1 = nums.value;
-  console.log(n1);
-  console.log(n1.split(""));
+  while (true) {}
 }
 
 function main() {
   console.log("Cacl1");
-  var out = document.getElementById("output");
-  out.innerHTML = "계산기 구현하기";
-  console.log(10 + 3);
-  console.log(10 * 3);
-  console.log(10 / 3);
-  console.log(10 - 3);
 }
 main();
